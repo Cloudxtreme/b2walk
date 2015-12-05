d3b53128f265cb0b9d9e1a2a6ff3411954d59cef
kbreit <kevin.breit@kevinbreit.net>
2015-12-05 14:00:11 -0600

    b2 script existance
    Check to see if the Backblaze b2 script exists and is executable.

--- b2walk.py
+++ b2walk.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import os
 import sys
 import subprocess
 import logging
@@ -8,9 +9,22 @@ import logging
 # b2walk and its developers take no responsibility for lost or corrupt data. There are no guarantees to go along with this script.
 # Syntax: b2walk -u username -p key -b bucket -d directory
 
-try:
-    b2Out = subprocess.check_output("b2") # Check to see if b2 exists
-except subprocess.CalledProcessError as b2Out: # This code doesn't actually work
-    if grepexc.returncode is 2:
-        print "b2 is not in the path"
-        sys.exit(1)
+
+
+def b2_exist(): # Check to see if the b2 script exists in the path and is executable
+    program = "b2"
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    for path in os.environ["PATH"].split(os.pathsep):
+        path = path.strip('"')
+        exe_file = os.path.join(path, program)
+        if is_exe(exe_file):
+            return exe_file
+    return 1
+
+b2_err = b2_exist()
+if b2_err is 1:
+    print "File doesn't exist"
+    return 1
