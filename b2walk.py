7ed034df2b6c5ed9bf245a8fcd45c4c65448cd31
kbreit <kevin.breit@kevinbreit.net>
2015-12-05 18:02:34 -0600

    Argument parsing support
    Initial support for argument parsing. So far it’s pretty straight
    forward but it probably doesn’t need to be overly complex today.

--- b2walk.py
+++ b2walk.py
@@ -4,6 +4,7 @@ import os
 import sys
 import subprocess
 import logging
+import argparse
 
 # b2walk is a script to intelligently backup a directory on Linux to Backblaze's b2 online storage. It does require a B2 account.
 # b2walk and its developers take no responsibility for lost or corrupt data. There are no guarantees to go along with this script.
@@ -11,7 +12,7 @@ import logging
 
 
 
-def b2_exist(): # Check to see if the b2 script exists in the path and is executable
+def b2_exist():
     program = "b2"
     def is_exe(fpath):
         return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
@@ -24,7 +25,23 @@ def b2_exist(): # Check to see if the b2 script exists in the path and is execut
             return exe_file
     return 1
 
+
+
+# Argument parsing section
+parser = argparse.ArgumentParser(description="b2walk will recursively backup files into Backblaze's B2 system")
+parser.add_argument('username', metavar='-u', type=str, nargs="+", help="Username provided by Backblaze")
+parser.add_argument('password', metavar='-k', type=str, nargs="+", help="Key provided by Backblaze")
+parser.add_argument('bucket', metavar='-b', type=str, nargs="+", help="Bucket to upload files to")
+parser.add_argument('directory', metavar='-d', type=str, nargs="+", help="Directory to recurisvely upload")
+args = parser.parse_args()
+
+user = args.username
+passw = args.password
+bucket = args.bucket
+dir = args.directory
+
+# Check if the file b2 exists and is executable
 b2_err = b2_exist()
 if b2_err is 1:
     print "File doesn't exist"
-    return 1
+    sys.exit(1)
